dc143d791ffa555ebfc680596ce47c9a43d952ec
ilijaaerodrom <ilija_trajkovski@outlook.com>
2022-04-21 20:59:52 +0200

    finish web fitness

---
+++ 10.Bootstrap/Ex 2/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Ilija Trajkovski
+    File Name: script.js 
+    Date: 21.04.2022
+*/
+
+// jquery for hero image to consume the header window space
+$(document).ready(function () {
+    $(".hero").height($(window).height())
+});
