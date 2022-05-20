b9463f8f48d69e7918dd30cac69a994213c9c302
ilijaaerodrom <ilija_trajkovski@outlook.com>
2022-05-20 12:47:03 +0200

    start with if

---
+++ Udemi/The Web Developer Bootcamp/Java script/If Statemnet/app.js
@@ -0,0 +1,29 @@
+
+// If Statement - only runs code if given condition is true
+
+let raiting = 3;
+    // Condition
+if (raiting === 3) {
+  console.log(`You are a Suoerstar`);
+}
+
+let random = Math.random();
+if (random < 0.5) {
+  console.log(`Your number is less than 0.5`);
+  console.log(random);
+}
+if (random >= 0.5) {
+  console.log(`Your number is greater than 0.5`);
+  console.log(random);
+}
+
+function isEvan(num) {
+   
+  
+  if (num = 4) {
+  console.log(`even`);
+  console.log(num);
+  }
+  
+}
+
