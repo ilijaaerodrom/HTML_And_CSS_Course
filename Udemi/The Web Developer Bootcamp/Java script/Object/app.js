cdd4710a3ce21e0390513f905fe2d0264c474305
ilijaaerodrom <ilija_trajkovski@outlook.com>
2022-05-25 14:39:55 +0200

    start with objects

---
+++ Udemi/The Web Developer Bootcamp/Java script/Object/app.js
@@ -0,0 +1,13 @@
+
+
+// Objects
+const person = {
+  firstName: `Ilija`,
+  lastName: `Trajkovski`,
+};
+console.log(person);
+
+// Accessing Data out of Objects
+person[`firstName`];
+console.log(person[`firstName`]);
+console.log(person.lastName);
