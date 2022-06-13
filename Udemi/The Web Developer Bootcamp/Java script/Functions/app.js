ea2df40d4d4845498ca3434305a2fb665ad3ea94
ilijaaerodrom <ilija_trajkovski@outlook.com>
2022-06-13 15:10:57 +0200

    first exercise

---
+++ Udemi/The Web Developer Bootcamp/Java script/Functions/app.js
@@ -0,0 +1,16 @@
+
+// Function
+
+function singSong() {
+  console.log(`do`);
+  console.log(`re`);
+  console.log(`mi`);
+}
+singSong();
+
+// Arguments
+
+function greet(firstName) {
+  console.log(`firstName is: ${firstName}` );
+}
+greet(`Ilija`);
